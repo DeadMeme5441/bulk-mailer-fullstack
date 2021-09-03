97be9e3fb239227aa02a64ddc899f289262d6bfb
DeadUnderscoreMeme <deadunderscorememe@gmail.com>
2021-09-03 15:46:33 +0530

    Added explanation readme

---
+++ server/README.md
@@ -0,0 +1,15 @@
+# Fullstack Mailer Project.
+
+This project contains a mailer that was built to better understand redux, redux-forms and passport.js.
+
+The project was built on the following stack
+
+1. Node
+2. Express
+3. Passport
+4. React
+5. Redux
+6. Redux-Forms
+7. Sendgrid-api for sending mails.
+
+TODO - Write the docker file for universal deployment.
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,3 @@
-// prod.js - production keys here!!
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -7,5 +6,5 @@ module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+  redirectDomain: process.env.REDIRECT_DOMAIN,
 };
